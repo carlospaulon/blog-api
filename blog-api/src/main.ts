17be2bfbc2d447187bb7618a1cbc932fb0455f57
Carlos <paulonluis222@gmail.com>
2025-10-30 15:17:42 -0300

    docs(swagger): setup Swagger module and configuration in main.ts

--- blog-api/src/main.ts
+++ blog-api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 
 async function bootstrap() {
@@ -21,6 +22,21 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('Personal Blog Platform REST API')
+    .setVersion('1.0')
+    .addTag('articles', 'Article Management endpoints')
+    .addServer('http://localhost:3000', 'Development Server')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory, {
+    customSiteTitle: 'Blog API Documentation',
+    customfavIcon: 'https://nestjs.com/img/logo-small.svg',
+    customCss: '.swagger-ui .topbar { display: none }',
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
