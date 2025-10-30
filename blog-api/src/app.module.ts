34642330786ed92713d6252bf078a5e5c11c4050
Carlos <paulonluis222@gmail.com>
2025-10-30 16:13:55 -0300

    fix(security): improve Throttler handling for rate limiting

--- blog-api/src/app.module.ts
+++ blog-api/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Article } from './articles/entities/article.entity';
+import { Article } from 'src/articles/entities/article.entity';
 import { ArticlesModule } from './articles/articles.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
@@ -14,12 +14,16 @@ import { APP_GUARD } from '@nestjs/core';
       envFilePath: '.env',
     }),
 
-    ThrottlerModule.forRoot([
-      {
-        ttl: 60000,
-        limit: 10,
-      },
-    ]),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => [
+        {
+          ttl: config.get<number>('THROTTLE_TTL') || 60000,
+          limit: config.get<number>('THROTTLE_LIMIT') || 10,
+        },
+      ],
+    }),
 
     TypeOrmModule.forRoot({
       type: 'postgres',
